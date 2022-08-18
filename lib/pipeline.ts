249270ec8c1d8ace5ba43452e2f3e31c8b129108
EC2 Default User <ec2-user@ip-172-31-20-146.us-west-2.compute.internal>
2022-08-18 18:34:04 +0000

    Setting up EKS Blueprints deployment pipeline

---
+++ lib/pipeline.ts
@@ -0,0 +1,37 @@
+// lib/pipeline.ts
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+
+export default class PipelineConstruct extends Construct {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps){
+    super(scope, id)
+
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
+
+    const blueprint = blueprints.EksBlueprint.builder()
+    .account(account)
+    .region(region)
+    .addOns()
+    .teams();
+  
+    blueprints.CodePipelineStack.builder()
+      .name("eks-blueprints-workshop-pipeline")
+      .owner("youngjeong46")
+      .repository({
+          repoUrl: 'testtesttest',
+          credentialsSecretName: 'github-token',
+          targetRevision: 'main'
+      })
+      .wave({
+        id: "envs",
+        stages: [
+          { id: "dev", stackBuilder: blueprint.clone('us-west-2')},
+          { id: "test", stackBuilder: blueprint.clone('us-east-2')},
+          { id: "prod", stackBuilder: blueprint.clone('us-east-1')}
+        ]
+      })
+      .build(scope, id+'-stack', props);
+  }
+}
